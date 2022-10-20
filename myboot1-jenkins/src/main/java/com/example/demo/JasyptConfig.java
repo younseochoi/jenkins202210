18963d3b53d434970f660ded72538e92ebd32788
younseochoi <younseo42@gmail.com>
2022-10-20 11:08:24 +0900

    db정보 수

--- myboot1-jenkins/src/main/java/com/example/demo/JasyptConfig.java
+++ myboot1-jenkins/src/main/java/com/example/demo/JasyptConfig.java
@@ -25,7 +25,11 @@ public class JasyptConfig {
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
         //config.setPassword("1234"); // 암호화 키
-        config.setPassword(environment.getProperty("jasypt.encryptor.password"));
+        // sts내 암호화비번
+        //config.setPassword(environment.getProperty("jasypt.encryptor.password"));
+        
+        //네이버 jenkins 
+        config.setPassword(System.getenv("DB_PASSWORD"));
         config.setAlgorithm("PBEWithMD5AndDES"); // 알고리즘
         config.setKeyObtentionIterations("1000");
         config.setPoolSize("1");
